be3c65fab077e1953630ab21884e4f1948cc81b8
Naruto TAKAHASHI <tnaruto@gmail.com>
2018-07-08 12:07:10 +0900

    split release_win.bat

---
+++ Scripts/build_converter_win.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+set CURDIR=%~dp0
+set BASEDIR=%CURDIR%..
+set BUILDDIR=%BASEDIR%\Build
+@echo on
+
+set BUILD_TYPE=Debug
+if not "%1" == "" (
+    set BUILD_TYPE=%1
+)
+pushd "%BUILDDIR%\Converter"
+rmdir /S /Q build
+mkdir build
+pushd build
+cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% .. || exit /b 1
+cmake --build . --target ALL_BUILD -- /p:Configuration=%BUILD_TYPE% || exit /b 1
+ctest -C %BUILD_TYPE% .
+popd
+popd
--- Scripts/release_win.bat
+++ Scripts/release_win.bat
@@ -5,34 +5,12 @@ set BASEDIR=%CURDIR%..
 set BUILDDIR=%BASEDIR%\Build
 set TOOLSDIR=%BASEDIR%\Tools
 set QT_PREFIX=C:\Qt\5.11.1\msvc2017_64
-set VCDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017
 @echo on
 
-cd "%BUILDDIR%\Converter"
-rmdir /S /Q build
-mkdir build
-cd build
-cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release .. || exit /b 1
-cmake --build . --target ALL_BUILD -- /p:Configuration=Release || exit /b 1
-ctest -C Release .
+call %CURDIR%\build_converter_win.bat Release
+call %CURDIR%\build_convertergui_win.bat Release
 
-if exist "%VCDIR%\Enterprise" (
-    set VCVARSALL="%VCDIR%\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
-) else if exist "%VCDIR%\Professional" (
-    set VCVARSALL="%VCDIR%\Professional\VC\Auxiliary\Build\vcvarsall.bat"
-) else (
-    set VCVARSALL="%VCDIR%\Community\VC\Auxiliary\Build\vcvarsall.bat"
-)
-
-call %VCVARSALL% x64
-cd "%BUILDDIR%\Ss6ConverterGUI\Ss6ConverterGUI"
-if exist Makefile (
-    nmake distclean
-)
-%QT_PREFIX%\bin\qmake Release=1 || exit /b 1
-nmake || exit /b 1
-
-cd %BASEDIR%
+pushd %BASEDIR%
 rmdir /S/Q Ss6Converter
 mkdir Ss6Converter
 copy %BUILDDIR%\Converter\build\Release\Ss6Converter.exe Ss6Converter\
@@ -41,3 +19,4 @@ copy %BUILDDIR%\Ss6ConverterGUI\Ss6ConverterGUI\Release\Ss6ConverterGUI.exe Ss6C
 zip -r Ss6Converter.zip Ss6Converter
 move Ss6Converter.zip %TOOLSDIR%\
 rmdir /S/Q Ss6Converter
+popd
