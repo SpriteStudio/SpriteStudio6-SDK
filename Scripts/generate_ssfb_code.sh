4d766dc4ce679e33bee6088b791465a3003d86eb
Naruto TAKAHASHI <tnaruto@gmail.com>
2018-10-06 21:49:51 +0900

    sbmodule の flatc から ssfb コードを生成するバッチ generate_ssfb_code.sh を追加

---
+++ Scripts/generate_ssfb_code.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -ex
+
+SCRIPTDIR=`dirname $0`
+SCRIPTDIR=`cd $SCRIPTDIR && pwd -P`
+BASEDIR=${SCRIPTDIR}/..
+BASEDIR=`cd ${BASEDIR} && pwd -P`
+BUILDDIR=${BASEDIR}/Build
+BUILDDIR=`cd ${BUILDDIR} && pwd -P`
+
+pushd ${BUILDDIR}
+
+pushd Converter
+
+# Build flatc
+pushd flatbuffers
+/bin/rm -rf build
+/bin/mkdir build
+
+pushd build 
+cmake ..
+cmake --build .
+FLATC=$(pwd)/flatc
+popd > /dev/null # build
+
+popd > /dev/null # flatbuffers
+
+# generate ssfb code
+${FLATC} -c fbs/ssfb.fbs
+
+popd > /dev/null # Converter
+popd > /dev/null # ${BUILDDIR}
