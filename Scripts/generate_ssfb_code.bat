5e9844607d306a389a5f506a76de86c02ee99f90
Naruto TAKAHASHI <tnaruto@gmail.com>
2018-10-13 19:45:17 +0900

    sbmodule の flatc から ssfb コードを生成するバッチ generate_ssfb_code.bat を追加

---
+++ Scripts/generate_ssfb_code.bat
@@ -0,0 +1,28 @@
+@echo off
+setlocal
+set CURDIR=%~dp0
+set BASEDIR=%CURDIR%..
+set BUILDDIR=%BASEDIR%\Build
+@echo on
+
+set BUILD_TYPE=Release
+if not "%1" == "" (
+    set BUILD_TYPE=%1
+)
+
+pushd "%BUILDDIR%\Converter"
+
+rem Build flatc
+pushd "flatbuffers"
+
+git clean -fdx .
+cmake -DCMAKE_BUILD_TYPE=%BUILD_TYPE% . || exit /b 1
+cmake --build . --target ALL_BUILD -- /p:Configuration=%BUILD_TYPE% || exit /b 1
+set FLATC="%BUILDDIR%\Converter\flatbuffers\%BUILD_TYPE%\flatc.exe"
+
+popd
+
+rem generate ssfb code
+%FLATC% -c fbs\ssfb.fbs
+
+popd
