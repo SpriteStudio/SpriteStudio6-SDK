24625ba3107ebed7862a7a191329d25a72fec513
Naruto TAKAHASHI <tnaruto@gmail.com>
2019-02-10 11:10:27 +0900

    [windows] build Viewer2 on cli

---
+++ Scripts/prepare_build_viewer2_win.bat
@@ -0,0 +1,24 @@
+@echo off
+setlocal
+set CURDIR=%~dp0
+set BASEDIR=%CURDIR%..
+set BUILDDIR=%BASEDIR%\Build
+set DEPENDSDIR=%BUILDDIR%\Depends
+@echo on
+
+pushd %BUILDDIR%\Viewer2
+rmdir /S /Q JUCE
+rmdir /S /Q FRUT
+git clone --branch=5.3.2 --depth=1 --single-branch https://github.com/WeAreROLI/JUCE.git || exit /b 1
+git clone https://github.com/McMartin/FRUT.git || exit /b 1
+
+rem create Jucer2Reprojucer command
+pushd FRUT
+mkdir build 
+pushd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../prefix -DJUCE_ROOT=..\..\JUCE
+cmake --build . --target install
+popd
+popd
+
+popd
