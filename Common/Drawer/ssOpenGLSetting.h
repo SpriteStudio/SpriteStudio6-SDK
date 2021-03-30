b842e364c9b49c87c9e93a32696447ef9581fcaf
Toshiyuki Kurooka <kurooka@miobox.jp>
2021-03-30 15:22:45 +0900

    Update ssOpenGLSetting.h
    MacでのGLヘッダ順序入れ替え

--- Common/Drawer/ssOpenGLSetting.h
+++ Common/Drawer/ssOpenGLSetting.h
@@ -6,17 +6,18 @@
 
 #ifdef USE_GLEW
 //#include <GL/GL.h>
+    #define GLEW_STATIC
+    #include <GL/glew.h>
 
-#ifndef _WIN32
-    #include <OpenGL/gl.h>
-    #include <OpenGL/glext.h>
-#endif
-#include <GL/glew.h>
-
+    #ifndef _WIN32
+        #include <OpenGL/gl.h>
+        #include <OpenGL/glext.h>
+    #endif
+//    #include <GL/gl.h>
 #else
-#include <glad/glad.h>
-//#include <glad/gl.h>
-#include <GLFW/glfw3.h>
+    #include <glad/glad.h>
+    //#include <glad/gl.h>
+    #include <GLFW/glfw3.h>
 #endif
 
 
