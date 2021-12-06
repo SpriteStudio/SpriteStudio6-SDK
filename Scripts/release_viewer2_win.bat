9952df0b01af4361b09a27117c65b6a09d7ffec8
Naruto TAKAHASHI <tnaruto@gmail.com>
2021-12-06 21:57:58 +0900

    update release_viewer2_win.bat

--- Scripts/release_viewer2_win.bat
+++ Scripts/release_viewer2_win.bat
@@ -12,7 +12,7 @@ call "%CURDIR%\build_viewer2_win.bat" Release || exit /b 1
 pushd %BASEDIR%
 rmdir /S /Q viewer_sample_2
 mkdir viewer_sample_2
-copy "%BUILDDIR%\Viewer2\cmakeBuild\Release\App\Viewer2.exe" viewer_sample_2\
+copy "%BUILDDIR%\Viewer2\cmakeBuild\SSView2_artefacts\Release\\SSViewer2.exe" viewer_sample_2\
 powershell compress-archive viewer_sample_2 viewer_sample_2.zip
 move /y viewer_sample_2.zip %TOOLSDIR%\
 rmdir /S /Q viewer_sample_2
