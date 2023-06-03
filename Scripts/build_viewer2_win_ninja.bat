00416a3d8c78933267a440c0f249993b3b234ba5
Naruto TAKAHASHI <tnaruto@gmail.com>
2023-06-03 15:54:33 +0900

    add cmake + ninja + msvc bat files

---
+++ Scripts/build_viewer2_win_ninja.bat
@@ -0,0 +1,41 @@
+@echo off
+setlocal
+set CURDIR=%~dp0
+set BASEDIR=%CURDIR%..
+set BUILDDIR=%BASEDIR%\Build
+set VCDIR=C:\Program Files\Microsoft Visual Studio\2022
+set Jucer2CMake=%BUILDDIR%\Viewer2\FRUT\prefix\FRUT\bin\Jucer2CMake.exe
+@echo on
+
+set BUILD_TYPE=Debug
+if not "%1" == "" (
+    set BUILD_TYPE=%1
+)
+
+for /f "tokens=2 delims==" %%I in (
+  'wmic cpu get architecture /value'
+) do set "cpuArch=%%I"
+if "%cpuArch%"=="12" (
+  set ARCH=arm64
+) else (
+  set ARCH=x64
+)
+
+if exist "%VCDIR%\Enterprise" (
+    set VCVARSALL="%VCDIR%\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
+) else if exist "%VCDIR%\Professional" (
+    set VCVARSALL="%VCDIR%\Professional\VC\Auxiliary\Build\vcvarsall.bat"
+) else (
+    set VCVARSALL="%VCDIR%\Community\VC\Auxiliary\Build\vcvarsall.bat"
+)
+
+call %VCVARSALL% %ARCH%
+
+pushd %BUILDDIR%\Viewer2
+rmdir /S /Q cmakeBuild
+mkdir cmakeBuild
+pushd cmakeBuild
+cmake -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -G Ninja .. || exit /b 1
+cmake --build . || exit /b 1
+popd
+popd
