156b2e61afcb6984720591393c80322e351c072b
Naruto TAKAHASHI <tnaruto@gmail.com>
2024-05-28 23:37:03 +0900

    execute fopen myself.

--- Common/Loader/ssloader_sspj.cpp
+++ Common/Loader/ssloader_sspj.cpp
@@ -12,25 +12,25 @@ namespace spritestudio6
 {
 
 
-SsString	SsProject::getSsceBasepath(){ 
+SsString	SsProject::getSsceBasepath(){
 	return getFullPath( m_proj_filepath , settings.cellMapBaseDirectory );
 }
 
 
 SsString	SsProject::getSsaeBasepath()
-{ 
+{
 	return getFullPath( m_proj_filepath , settings.animeBaseDirectory );
 }
 
 
 SsString	SsProject::getSseeBasepath()
-{ 
+{
 	return getFullPath( m_proj_filepath , settings.effectBaseDirectory);
 }
 
 
 SsString	SsProject::getSsqeBasepath()
-{ 
+{
 	return getFullPath( m_proj_filepath , settings.animeBaseDirectory );
 }
 
@@ -156,135 +156,142 @@ SsProject*	ssloader_sspj::Parse_ProjectOnly(const char* xmlstr , size_t len)
 	}
 }
 
-SsProject*	ssloader_sspj::Load(const std::string& filename )
-{
+SsProject*	ssloader_sspj::Load(const std::string& filename ) {
 	libXML::XMLDocument xml;
-	std::string filenameSSPJ = SsCharConverter::convert_path_string( filename );
 
-	if ( libXML::XML_SUCCESS == xml.LoadFile( filenameSSPJ.c_str() ) )
+	FILE *fp = 0;
+#if _WIN32 || _WIN64
+    std::wstring wfilesspj = SsCharConverter::convert_path_string( filename );
+    _wfopen_s(&fp, wfilesspj.c_str(), L"rb");
+#else
+	fp = fopen( SsCharConverter::convert_path_string( filename ).c_str(), "rb" );
+#endif
+
+	libXML::XMLError result = xml.LoadFile(fp);
+	fclose(fp);
+	if (result != libXML::XML_SUCCESS) {
+		return 0;
+	}
+
+	SsXmlIArchiver ar( xml.GetDocument() , "SpriteStudioProject" );
+
+	SsProject* proj = new SsProject();
+	proj->__Serialize( &ar );
+	//		std::string project_filepath = SsCharConverter::convert_path_string( path2dir( filename ) );
+	std::string project_filepath = path2dir( filename );
+	proj->setFilepath( project_filepath );
+
+	if ( checkFileVersion(proj->version, SPRITESTUDIO6_SSPJVERSION) == false )
 	{
-		SsXmlIArchiver ar( xml.GetDocument() , "SpriteStudioProject" );
+		DEBUG_PRINTF("Project load error : %s", project_filepath.c_str());
+		DEBUG_PRINTF("sspj old version");
+		delete proj;
+		return 0;
+	}
 
-		SsProject* proj = new SsProject();
-		proj->__Serialize( &ar );
-//		std::string project_filepath = SsCharConverter::convert_path_string( path2dir( filename ) );
-		std::string project_filepath = path2dir( filename );
-		proj->setFilepath( project_filepath );
-
-		if ( checkFileVersion(proj->version, SPRITESTUDIO6_SSPJVERSION) == false )
+	//アニメーションリストを元に読み込みます。
+	for ( size_t i = 0 ;i < proj->getAnimePackNum() ; i++ )
+	{
+		SsString ssaepath = SsCharConverter::convert_path_string(proj->getAnimePackFilePath(i));
+		SsAnimePack* anime = ssloader_ssae::Load( ssaepath );
+		if ( ( anime ) && ( checkFileVersion(anime->version, SPRITESTUDIO6_SSAEVERSION) == true ) )
 		{
-			DEBUG_PRINTF("Project load error : %s", project_filepath.c_str());
-			DEBUG_PRINTF("sspj old version");
+			proj->animeList.push_back( std::move( std::unique_ptr<SsAnimePack>( anime ) ) );
+		}else{
+			//エラー
+			DEBUG_PRINTF( "Animation load error : %s" , ssaepath.c_str() );
+			DEBUG_PRINTF( "ssae old version" );
+
+			if ( anime ) delete anime;
 			delete proj;
 			return 0;
 		}
+	}
 
-		//アニメーションリストを元に読み込みます。
-		for ( size_t i = 0 ;i < proj->getAnimePackNum() ; i++ )
+	std::map<std::string, std::string> textures;
+
+	//セルマップリストを元に読み込みます。
+	for ( size_t i = 0 ;i < proj->getCellMapNum() ; i++ )
+	{
+		SsString sscepath = SsCharConverter::convert_path_string(proj->getCellMapFilePath(i));
+		SsCellMap* cell = ssloader_ssce::Load( sscepath );
+		cell->loadFilepath = proj->getCelMapFileOriginalPath(i);
+		proj->cellmapList.push_back( std::move( std::unique_ptr<SsCellMap>( cell ) ) );
+
+		//セルマップリストからテクスチャを取得
+		textures[cell->imagePath] = sscepath;
+		/*
+					if ( ( cell ) && (checkFileVersion(cell->version, SPRITESTUDIO6_SSCEVERSION) == true) )
+					{
+						cell->loadFilepath = proj->getCelMapFileOriginalPath(i);
+						proj->cellmapList.push_back( cell );
+					}else{
+						//エラー
+						DEBUG_PRINTF( "Cellmap load error : %s" , sscepath.c_str() );
+						DEBUG_PRINTF( "ssce old version" );
+						delete proj;
+						return 0;
+					}
+		*/
+	}
+
+	//テクスチャリストを保存
+	proj->textureList.clear();
+	for (auto i = textures.begin(); i != textures.end(); i++)
+	{
+		proj->textureList.push_back(i->first);
+	}
+
+	//エフェクトリストを元に読み込みます。
+	for ( size_t i = 0 ;i < proj->getEffectFileNum() ; i++ )
+	{
+		SsString sscepath = SsCharConverter::convert_path_string(proj->getEffectFilePath(i));
+		SsEffectFile* efile = ssloader_ssee::Load( sscepath );
+		ssloader_ssee::loadPostProcessing( efile, proj );
+		proj->effectfileList.push_back( std::move( std::unique_ptr<SsEffectFile>( efile ) ) );
+		/*
+					if ( ( efile ) && ( checkFileVersion(efile->version, SPRITESTUDIO6_SSCEVERSION) == true ) )
+					{
+						//efile->loadFilepath = proj->getCelMapFileOriginalPath(i);
+						proj->effectfileList.push_back( efile );
+						ssloader_ssee::loadPostProcessing( efile , proj );
+					}else{
+						//エラー
+						DEBUG_PRINTF( "effect load error : %s" , sscepath.c_str() );
+						DEBUG_PRINTF( "ssee old version" );
+						delete proj;
+						return 0;
+					}
+		*/
+
+	}
+
+	//シーケンスリストを元に読み込みます。
+	for ( size_t i = 0 ;i < proj->getSequencePackNum() ; i++ )
+	{
+		SsString ssqepath = SsCharConverter::convert_path_string(proj->getSequencePackFilePath(i));
+		SsSequencePack* sequence = ssloader_ssqe::Load( ssqepath );
+		if ( ( sequence ) && ( checkFileVersion( sequence->version, SPRITESTUDIO6_SSQEVERSION) == true ) )
 		{
-			SsString ssaepath = SsCharConverter::convert_path_string(proj->getAnimePackFilePath(i));
-			SsAnimePack* anime = ssloader_ssae::Load( ssaepath );
-			if ( ( anime ) && ( checkFileVersion(anime->version, SPRITESTUDIO6_SSAEVERSION) == true ) ) 
-			{
-				proj->animeList.push_back( std::move( std::unique_ptr<SsAnimePack>( anime ) ) );
-			}else{
-				//エラー
-				DEBUG_PRINTF( "Animation load error : %s" , ssaepath.c_str() );
-				DEBUG_PRINTF( "ssae old version" );
+			proj->sequenceList.push_back( std::move( std::unique_ptr<SsSequencePack>( sequence ) ) );
+		}else{
+			//エラー
+			DEBUG_PRINTF( "Sequence load error : %s" , ssqepath.c_str() );
+			DEBUG_PRINTF( "ssqe old version" );
 
-				if ( anime ) delete anime;
-				delete proj;
-				return 0;
-			}
+			if ( sequence ) delete sequence;
+			delete proj;
+			return 0;
 		}
+	}
 
-		std::map<std::string, std::string> textures;
-
-		//セルマップリストを元に読み込みます。
-		for ( size_t i = 0 ;i < proj->getCellMapNum() ; i++ )
-		{
-			SsString sscepath = SsCharConverter::convert_path_string(proj->getCellMapFilePath(i));
-			SsCellMap* cell = ssloader_ssce::Load( sscepath );
-			cell->loadFilepath = proj->getCelMapFileOriginalPath(i);
-			proj->cellmapList.push_back( std::move( std::unique_ptr<SsCellMap>( cell ) ) );
-
-			//セルマップリストからテクスチャを取得
-			textures[cell->imagePath] = sscepath;
-/*
-			if ( ( cell ) && (checkFileVersion(cell->version, SPRITESTUDIO6_SSCEVERSION) == true) )
-			{
-				cell->loadFilepath = proj->getCelMapFileOriginalPath(i);
-				proj->cellmapList.push_back( cell );
-			}else{
-				//エラー
-				DEBUG_PRINTF( "Cellmap load error : %s" , sscepath.c_str() );
-				DEBUG_PRINTF( "ssce old version" );
-				delete proj;
-				return 0;
-			}
-*/
-		}
-
-		//テクスチャリストを保存
-		proj->textureList.clear();
-		for (auto i = textures.begin(); i != textures.end(); i++)
-		{
-			proj->textureList.push_back(i->first);
-		}
-
-		//エフェクトリストを元に読み込みます。
-		for ( size_t i = 0 ;i < proj->getEffectFileNum() ; i++ )
-		{
-			SsString sscepath = SsCharConverter::convert_path_string(proj->getEffectFilePath(i));
-			SsEffectFile* efile = ssloader_ssee::Load( sscepath );
-			ssloader_ssee::loadPostProcessing( efile, proj );
-			proj->effectfileList.push_back( std::move( std::unique_ptr<SsEffectFile>( efile ) ) );
-/*
-			if ( ( efile ) && ( checkFileVersion(efile->version, SPRITESTUDIO6_SSCEVERSION) == true ) )
-			{
-				//efile->loadFilepath = proj->getCelMapFileOriginalPath(i);
-				proj->effectfileList.push_back( efile );
-				ssloader_ssee::loadPostProcessing( efile , proj );
-			}else{
-				//エラー
-				DEBUG_PRINTF( "effect load error : %s" , sscepath.c_str() );
-				DEBUG_PRINTF( "ssee old version" );
-				delete proj;
-				return 0;
-			}
-*/
-
-		}
-
-		//シーケンスリストを元に読み込みます。
-		for ( size_t i = 0 ;i < proj->getSequencePackNum() ; i++ )
-		{
-			SsString ssqepath = SsCharConverter::convert_path_string(proj->getSequencePackFilePath(i));
-			SsSequencePack* sequence = ssloader_ssqe::Load( ssqepath );
-			if ( ( sequence ) && ( checkFileVersion( sequence->version, SPRITESTUDIO6_SSQEVERSION) == true ) ) 
-			{
-				proj->sequenceList.push_back( std::move( std::unique_ptr<SsSequencePack>( sequence ) ) );
-			}else{
-				//エラー
-				DEBUG_PRINTF( "Sequence load error : %s" , ssqepath.c_str() );
-				DEBUG_PRINTF( "ssqe old version" );
-
-				if ( sequence ) delete sequence;
-				delete proj;
-				return 0;
-			}
-		}
-
-		return proj;
-	}	
-
-	return 0;
+	return proj;
 }
 
 SsCellMap* SsProject::findCellMap( SsString& str )
 {
-	for ( SsSsCellMapListItr itr = cellmapList.begin() ; 
-		itr != cellmapList.end() ; itr ++) 
+	for ( SsSsCellMapListItr itr = cellmapList.begin() ;
+		itr != cellmapList.end() ; itr ++)
 	{
 //		SsString _name = (*itr)->name;
 		//_name+=".ssce";
