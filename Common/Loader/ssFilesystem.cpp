5d151c6fb43c7dfcf93df5fcee36af3f5ac30973
Naruto TAKAHASHI <tnaruto@gmail.com>
2024-06-05 00:25:16 +0900

    build fix on Windows.

--- Common/Loader/ssFilesystem.cpp
+++ Common/Loader/ssFilesystem.cpp
@@ -10,9 +10,9 @@ using namespace spritestudio6;
 FILE *ssFilesystem::openFile(const std::string &filepath) {
     FILE *fp = NULL;
 #if _WIN32 || _WIN64
-    int wchars_num = MultiByteToWideChar(CP_UTF8, 0, filename.c_str(), -1, NULL, 0);
+    int wchars_num = MultiByteToWideChar(CP_UTF8, 0, filepath.c_str(), -1, NULL, 0);
     std::wstring wstr(wchars_num, 0);
-    MultiByteToWideChar(CP_UTF8, 0, filename.c_str(), -1, &wstr[0], wchars_num);
+    MultiByteToWideChar(CP_UTF8, 0, filepath.c_str(), -1, &wstr[0], wchars_num);
 
     _wfopen_s(&fp, wstr.c_str(), L"rb");
 #else
