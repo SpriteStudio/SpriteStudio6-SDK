c71030a9c26e0d21c58c361c41eca284b3866c2b
Naruto TAKAHASHI <tnaruto@gmail.com>
2024-06-16 11:41:35 +0900

    change logic of converter error handling.

--- Build/Converter/SsPlayerConverter.h
+++ Build/Converter/SsPlayerConverter.h
@@ -5,8 +5,7 @@
 #ifndef _SsPlayerConverter_h
 #define _SsPlayerConverter_h
 
-enum SsPlayerConverterResultCode
-{
+enum SsPlayerConverterResultCode {
     SSPC_SUCCESS = 0,                   // 成功
     SSPC_INTERNAL_ERROR,                // 内部エラーが発生しました（※バグなどの不具合が無い限り通常発生しません）
     SSPC_NOT_SPECIFY_INPUT_FILE,        // 入力ファイルの指定がありません
@@ -16,7 +15,19 @@ enum SsPlayerConverterResultCode
     SSPC_IMPOSSIBLE_PLURAL_SSF_FILES,   // ssfファイルが複数指定されています。一つのみ指定可能です
     SSPC_SSAX_PARSE_FAILED,             // ssaxファイルの解析に失敗しました
     SSPC_SSPJ_PARSE_FAILED,             // sspjファイルの解析に失敗しました
-    
+
+    SSPC_INCOMPATIBLE_VERSION_ERROR,    // 対応しないバージョン
+    SSPC_PARTS_ERROR,                   //
+    SSPC_NOT_EXIST_OUTPUT_DIR,          // 出力ディレクトリがありません
+    SSPC_NOT_EXIST_CELLMAP,             // セルマップが存在しません。
+    SSPC_NOT_CELL_IN_CELLMAP,           // セルマップにセルが存在しません。
+    SSPC_NOT_ANIMATION,                 // アニメーションが存在しません
+    SSPC_NOT_OUTPUT_FILE,               // 出力ファイルが開けません。
+    SSPC_SSPKG_ERROR,                   // SSPKG エラー
+    SSPC_ZENKAKU_ERROR,                 // 全角エラー
+    SSPC_UNSUPPORT_FORMAT,              // 未対応のフォーマット
+    SSPC_UNSUPPORT_EFFECT_COMMAND,      // 未対応のエフェクトコマンド
+
     END_OF_SSPC
 };
 
